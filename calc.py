f199322e5288c20cc30ee705607863bf7dee1e5b
sonhoseong <son809616@gmail.com>
2023-04-13 23:51:20 +0900

    빼기 곱하기 나누기 추가

--- calc.py
+++ calc.py
@@ -3,19 +3,51 @@
 def plus(x, y):
     return x+y
 
+def minus(x, y):
+	return x-y
+
+def multiply(x, y):
+	return x*y
+
+def division(x, y):
+	if y==0:
+		print("0으로 나눌 수 없습니다.")
+	else:
+		return x/y
+
 # main function
 def main():
     check = 1
     print("Welcome to calcuator")
     while check >= 1:        
-        print("0: exit, 1: plus")
+        print("0: exit, 1: plus, 2: minus, 3: multiply, 4: division")
         check = int(input())
         if check == 1:
             print("First Number")
             x = int(input())
             print("Second Number")
             y = int(input())
-            print("answer : ", plus(x,y))
+
+        elif check == 2:
+            print("First Number")
+            x = int(input())
+            print("Second Number")
+            y = int(input())
+            print("answer : ", minus(x, y))
+
+        elif check == 3:
+            print("First Number")
+            x = int(input())
+            print("Second Number")
+            y = int(input())
+            print("answer : ", multiply(x, y))
+
+        elif check == 4:
+            print("First Number")
+            x = int(input())
+            print("Second Number")
+            y = int(input())
+            print("answer : ", division(x, y))
         elif check > 1:
             print("Unsupported")
         else:
