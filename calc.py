b7deb006397a386855f0592209e46b35b190565a
EC-T1 <130280111+EC-T1@users.noreply.github.com>
2023-04-13 22:36:44 +0900

    Update calc.py

--- calc.py
+++ calc.py
@@ -7,19 +7,22 @@ def plus(x, y):
 def main():
     check = 1
     print("Welcome to calcuator")
-    while check >= 1:        
-        print("0: exit, 1: plus")
-        check = int(input())
-        if check == 1:
-            print("First Number")
-            x = int(input())
-            print("Second Number")
-            y = int(input())
-            print("answer : ", plus(x,y))
-        elif check > 1:
-            print("Unsupported")
-        else:
-            print("Thank you")
+    while check >= 1:
+        try:
+            print("0: exit, 1: plus")
+            check = int(input())
+            if check == 1:
+                print("First Number")
+                x = float(input())
+                print("Second Number")
+                y = float(input())
+                print("answer : ", plus(x,y))
+            elif check > 1:
+                print("Unsupported")
+            else:
+                print("Thank you")
+        except valueError:
+            print("제대로 된 숫자를 입력하세요")
 
 if __name__ == "__main__":
     main()
